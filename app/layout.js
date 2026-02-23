99f730ad4b5d3f060c8e542c2f02684a6b4cb36a
beecoming <danqing.zhou@beecoming.com>
2026-02-22 21:34:25 -0800

    feat: add Next.js wrapper for static site

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Beecoming"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
